08ab33549e53d44d6ee3dce80979dd8fc2e46b4a
liproskurina <liproskurina@ozon.ru>
2022-09-21 20:02:31 +0300

    run tests in Jenkins

--- tests.sh
+++ tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DOCKER_SCAN_SUGGEST=false docker build -t tests .
+DOCKER_SCAN_SUGGEST=false docker build -t opencard_tests .
 
-docker run --name tests_run --network $network tests pytest tests/lesson_5 --alluredir=allure-results  --url $url --browser $browser --browser_version $browser_version --executor $executor -n $threads
+docker run --name opencard_tests_run --network $network tests pytest tests/lesson_5 --alluredir=allure-results  --url $url --browser $browser --browser_version $browser_version --executor $executor -n $threads
 
-docker cp tests_run:/app/allure-results .
+docker cp opencard_tests_run:/app/allure-results .
 
-docker rm tests_run
+docker rm opencard_tests_run
